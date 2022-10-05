729a3334cfd42798a2a5113a1ab47b1baf9d76a5
Jesushs4 <lagt5799@gmail.com>
2022-10-05 12:33:07 +0200

    commit1

---
+++ Ejercicio2.java
@@ -0,0 +1,6 @@
+public class Ejercicio2 {
+    public static void main (String args[]) {
+        System.out.print("Nombre: Jesus \nDireccion: Joaquin Turina \nTelefono: 601434609");
+    }
+    
+}
