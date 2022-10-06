d9df2ac0419d29f6bd87d44fc37779550abbd10a
Jesushs4 <lagt5799@gmail.com>
2022-10-06 10:17:07 +0200

    Ej5 fix

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -3,13 +3,13 @@ public class Ejercicio5 {
         System.out.println("┌────────┬──────────┬────────────┬────────────┬────────────┬────────────┐");
         System.out.println("│  Hora  │  Lunes   │   Martes   │  Miercoles │   Jueves   │   Viernes  │");
         System.out.println("├────────┼──────────┼────────────┼────────────┼────────────┼────────────┤");
-        System.out.println("|  8:15  | \033[30m Program.|Sist.Inform.|Ent. de des.|  Program.  |     FOL    |");
-        System.out.println("|  9:15  | Program. |Sist.Inform.|Ent. de des.|  Program.  |     FOL    |");
-        System.out.println("|  10:15 |Leng.Marc.|Sist.Inform.|  Program.  | Leng.Marc. |     FOL    |");
-        System.out.println("|  11:15 |  Recreo  |   Recreo   |   Recreo   |   Recreo   |   Recreo   |");
-        System.out.println("|  11:45 |Leng.Marc.|  Program.  |  Program.  | Leng.Marc. |Sist.Inform.|");
-        System.out.println("|  12:45 |   BBDD   |  Program.  |    BBDD    |    BBDD    |Sist.Inform.|");
-        System.out.println("|  13:45 |   BBDD   |Ent. de des.|    BBDD    |    BBDD    |Sist.Inform.|");
+        System.out.println("|  8:15  | \033[32m Program.\033[37m|\033[31mSist.Inform.\033[37m|\033[34mEnt. de des.\033[37m|  \033[32mProgram.\033[37m  |     \033[33mFOL    \033[37m|");
+        System.out.println("|  9:15  \033[37m| \033[32mProgram. \033[37m|\033[31mSist.Inform.\033[37m|\033[34mEnt. de des.\033[37m|  \033[32mProgram.  \033[37m|     \033[33mFOL    \033[37m|");
+        System.out.println("|  10:15 \033[37m|\033[36mLeng.Marc.\033[37m|\033[31mSist.Inform.\033[37m|  \033[32mProgram.  \033[37m| \033[36mLeng.Marc. \033[37m|     \033[33mFOL    \033[37m|");
+        System.out.println("|  11:15 \033[37m|  Recreo  \033[37m|   Recreo   \033[37m|   Recreo   \033[37m|   Recreo   \033[37m|   Recreo   \033[37m|");
+        System.out.println("|  11:45 \033[37m|\033[36mLeng.Marc.\033[37m|  \033[32mProgram.  \033[37m|  \033[32mProgram.  \033[37m| \033[36mLeng.Marc. \033[37m|\033[31mSist.Inform.\033[37m|");
+        System.out.println("|  12:45 \033[37m|   \033[35mBBDD   \033[37m|  \033[32mProgram.  \033[37m|    \033[35mBBDD    \033[37m|    \033[35mBBDD    \033[37m|\033[31mSist.Inform.\033[37m|");
+        System.out.println("|  13:45 \033[37m|   \033[35mBBDD   \033[37m|\033[34mEnt. de des.\033[37m|    \033[35mBBDD    \033[37m|    \033[35mBBDD    \033[37m|\033[31mSist.Inform.\033[37m|");
         System.out.println("└────────┴──────────┴────────────┴────────────┴────────────┴────────────┘");
     }
 }
