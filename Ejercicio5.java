cadf5ef992395b2644dcadb9a18302f8eaca260c
Jesushs4 <lagt5799@gmail.com>
2022-10-06 10:08:58 +0200

    Ejercicios 3-8

---
+++ Ejercicio5.java
@@ -0,0 +1,15 @@
+public class Ejercicio5 {
+    public static void main (String args[]) {
+        System.out.println("┌────────┬──────────┬────────────┬────────────┬────────────┬────────────┐");
+        System.out.println("│  Hora  │  Lunes   │   Martes   │  Miercoles │   Jueves   │   Viernes  │");
+        System.out.println("├────────┼──────────┼────────────┼────────────┼────────────┼────────────┤");
+        System.out.println("|  8:15  | \033[30m Program.|Sist.Inform.|Ent. de des.|  Program.  |     FOL    |");
+        System.out.println("|  9:15  | Program. |Sist.Inform.|Ent. de des.|  Program.  |     FOL    |");
+        System.out.println("|  10:15 |Leng.Marc.|Sist.Inform.|  Program.  | Leng.Marc. |     FOL    |");
+        System.out.println("|  11:15 |  Recreo  |   Recreo   |   Recreo   |   Recreo   |   Recreo   |");
+        System.out.println("|  11:45 |Leng.Marc.|  Program.  |  Program.  | Leng.Marc. |Sist.Inform.|");
+        System.out.println("|  12:45 |   BBDD   |  Program.  |    BBDD    |    BBDD    |Sist.Inform.|");
+        System.out.println("|  13:45 |   BBDD   |Ent. de des.|    BBDD    |    BBDD    |Sist.Inform.|");
+        System.out.println("└────────┴──────────┴────────────┴────────────┴────────────┴────────────┘");
+    }
+}
